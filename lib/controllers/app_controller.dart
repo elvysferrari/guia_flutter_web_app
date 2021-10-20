767b5b607049cfffdb737fd400ded7aa0b158afe
elvysferrari <elvys.ferrari@gmail.com>
2021-10-20 13:27:49 -0300

    novas coisas

--- lib/controllers/app_controller.dart
+++ lib/controllers/app_controller.dart
@@ -2,4 +2,56 @@ import 'package:get/get.dart';
 
 class AppController extends GetxController{
   static AppController instance = Get.find();
+
+  List<FiquePorDentro> getListMobileItemsBottom(){
+    List<FiquePorDentro> _items = [];
+
+    List<FiquePorDentroItem> items1 = [];
+    List<FiquePorDentroItem> items2 = [];
+    List<FiquePorDentroItem> items3 = [];
+
+    FiquePorDentroItem item1 = FiquePorDentroItem(url: "", titulo: "Opções de contato", descricao: "Saiba como entrar em contato");
+    items1.add(item1);
+
+    FiquePorDentroItem item2 = FiquePorDentroItem(url: "", titulo: "Central de Ajuda", descricao: "Ajuda");
+    items1.add(item2);
+
+    FiquePorDentro hospedes = FiquePorDentro(titulo: "Para os usuários", items: items1);
+    _items.add(hospedes);
+
+    FiquePorDentroItem item3 = FiquePorDentroItem(url: "", titulo: "Recursos de banners", descricao: "Veja como criar");
+    items2.add(item3);
+
+    FiquePorDentroItem item4 = FiquePorDentroItem(url: "", titulo: "Como responder usuários", descricao: "Saiba como responder um contato");
+    items2.add(item4);
+
+    FiquePorDentro anfitrioes = FiquePorDentro(titulo: "Para os parceiros", items: items2);
+
+    _items.add(anfitrioes);
+
+    FiquePorDentroItem item5 = FiquePorDentroItem(url: "", titulo: "Guia Comercial 2021", descricao: "Conheça os principais recursos");
+    items3.add(item5);
+
+    FiquePorDentro mais = FiquePorDentro(titulo: "Mais", items: items3);
+    _items.add(mais);
+
+    return _items;
+  }
+}
+
+class FiquePorDentro{
+  String titulo;
+  List<FiquePorDentroItem> items;
+
+  FiquePorDentro({required this.titulo, required this.items});
+
+}
+
+class FiquePorDentroItem{
+  String url;
+  String titulo;
+  String descricao;
+
+  FiquePorDentroItem({required this.url, required this.titulo, required this.descricao});
+
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:guia_flutter_web_app/constants/app_themes.dart';
+import 'package:guia_flutter_web_app/constants/controllers.dart';
 import 'package:guia_flutter_web_app/constants/responsive.dart';
+import 'package:guia_flutter_web_app/controllers/app_controller.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -26,7 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
-        backgroundColor: Colors.grey[400],
+        backgroundColor: Colors.white,
         body: Responsive(
           mobile: HomeMobile(scrollController: _scrollController,),
           desktop: HomeDesktop(scrollController: _scrollController,),
@@ -40,13 +41,16 @@ class HomeMobile extends StatelessWidget {
 
   final TrackingScrollController scrollController;
 
-  const HomeMobile({
+  HomeMobile({
     Key? key,
     required this.scrollController
   }) : super(key: key);
 
+  final List<FiquePorDentro> _itemsFiquePorDentro = appController.getListMobileItemsBottom();
+
   @override
   Widget build(BuildContext context) {
+
     return CustomScrollView(
       controller: scrollController,
       slivers: [
@@ -66,7 +70,7 @@ class HomeMobile extends StatelessWidget {
                     width: double.infinity,
                     child: CupertinoTextField(
                       keyboardType: TextInputType.text,
-                      placeholder: 'Para onde você quer ir?',
+                      placeholder: 'O que procuras amigo?',
                       placeholderStyle: const TextStyle(
                         color: Color(0xffC4C6CC),
                         fontSize: 14.0,
@@ -77,7 +81,7 @@ class HomeMobile extends StatelessWidget {
                         EdgeInsets.fromLTRB(9.0, 6.0, 9.0, 6.0),
                         child: Icon(
                           Icons.search,
-                          color: Color(0xffC4C6CC),
+                          color: AppColors.primary,
                         ),
                       ),
                       decoration: BoxDecoration(
@@ -93,52 +97,306 @@ class HomeMobile extends StatelessWidget {
         ),
         SliverToBoxAdapter(
           child: Container(
-            height: 80,
+            height: 60,
             color: Colors.white,
             child: const Padding(
               padding: EdgeInsets.only(left: 18, right: 18),
-              child: Text("Explore destinos perto de você", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+              child: Text("Guia Comercial", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
             ),
           )
         ),
 
         //Lista de Explorar destinos
         SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: Container(
+              color: Colors.white,
+              height: 250.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return SizedBox(
+                    width: 200.0,
+                    child: Card(
+                      elevation: 0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: CachedNetworkImage(
+                                imageUrl: "https://blog.lexos.com.br/wp-content/uploads/2018/07/como-montar-uma-loja-de-roupas-1280x720.jpg",
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            flex: 2,
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 6.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Expanded(child: Text("Lojas", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),)),
+                                  Expanded(child:Text("5 minutos de carro", style: TextStyle(fontWeight: FontWeight.normal, fontSize: 10),)),
+                                ],
+                              ),
+                            ),
+                            flex: 1,
+                          ),
+
+                        ],
+                      ),
+
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.only(top: 30),
+          sliver: SliverToBoxAdapter(
+              child: Container(
+                height: 30,
+                color: Colors.white,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 20, right: 18),
+                  child: Text("Turismo e Coisas para Fazer", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+                ),
+              )
+          ),
+        ),
+        //Lista de Experiencias
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 18.0),
+            child: Container(
+              color: Colors.white,
+              height: 250.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return SizedBox(
+                    width: 200.0,
+                    child: Card(
+                      elevation: 0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: CachedNetworkImage(
+                                  imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbtwvthT6W_4lJ_r6vyrtA-yjpb9FWrtcLxQ&usqp=CAU",
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            flex: 2,
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 6.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Expanded(child: Text("Cachoeiras", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),)),
+                                  Expanded(child:Text("Lindas cachoeiras", style: TextStyle(fontWeight: FontWeight.normal, fontSize: 12),)),
+                                ],
+                              ),
+                            ),
+                            flex: 1,
+                          ),
+                        ],
+                      ),
+
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.only(top: 30),
+          sliver: SliverToBoxAdapter(
+              child: Container(
+                height: 30,
+                color: Colors.white,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 20, right: 18),
+                  child: Text("Serviços", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+                ),
+              )
+          ),
+        ),
+        //Lista de Experiencias
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 18.0),
+            child: Container(
+              color: Colors.white,
+              height: 250.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return SizedBox(
+                    width: 200.0,
+                    child: Card(
+                      elevation: 0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: CachedNetworkImage(
+                                imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH47XTnCklo9A8z9wisrPHojkx0xaFbwAnCg&usqp=CAU",
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            flex: 2,
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 6.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Expanded(child: Text("Eletricista", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),)),
+                                  Expanded(child:Text("Encontre um profissional", style: TextStyle(fontWeight: FontWeight.normal, fontSize: 12),)),
+                                ],
+                              ),
+                            ),
+                            flex: 1,
+                          ),
+                        ],
+                      ),
+
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.only(top: 30),
+          sliver: SliverToBoxAdapter(
+              child: Container(
+                height: 30,
+                color: Colors.white,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 20, right: 18),
+                  child: Text("Anúncios dos Usuários", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+                ),
+              )
+          ),
+        ),
+        //Lista de Experiencias
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 18.0),
+            child: Container(
+              color: Colors.white,
+              height: 250.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return SizedBox(
+                    width: 200.0,
+                    child: Card(
+                      elevation: 0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: CachedNetworkImage(
+                                imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMnz2sgXiJRIPzj66kGL9NPnWE5K0tVqMQgg&usqp=CAU",
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            flex: 2,
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 6.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Expanded(child: Text("Automóveis", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),)),
+                                  Expanded(child:Text("Carros e motos", style: TextStyle(fontWeight: FontWeight.normal, fontSize: 12),)),
+                                ],
+                              ),
+                            ),
+                            flex: 1,
+                          ),
+                        ],
+                      ),
+
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.only(top: 30),
+          sliver: SliverToBoxAdapter(
+              child: Container(
+                height: 60,
+                color: Colors.grey[100],
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 20, right: 18, top: 18),
+                  child: Text("Fique por dentro", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+                ),
+              )
+          ),
+        ),
+        //Lista Fique por dentro
+        SliverToBoxAdapter(
           child: Container(
-            color: Colors.white,
-            height: 150.0,
+            color: Colors.grey[100],
+            height: 250.0,
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemCount: 10,
+              itemCount: _itemsFiquePorDentro.length,
               itemBuilder: (context, index) {
                 return SizedBox(
-                  width: 150.0,
+                  width: 250.0,
                   child: Card(
+                    color: Colors.grey[100],
                     elevation: 0,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Expanded(
-                          child: Container(
-                          decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50)), child: CachedNetworkImage(imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDZMsLbi05ZNE2JEkQpIXPLdpSZtk-YL6dIA&usqp=CAU")),
-                          flex: 2,
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: const [
-                                Expanded(child: Text("Pato Branco", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),)),
-                                Expanded(child:Text("5 minutos de carro", style: TextStyle(fontWeight: FontWeight.normal, fontSize: 10),)),
-                              ],
-                            ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16),
+                          child: SizedBox(
+                            height: 30,
+                            child: Text(_itemsFiquePorDentro[index].titulo, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.normal),)
                           ),
-                          flex: 1,
                         ),
-                        
-                      ],
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: _itemsFiquePorDentro[index].items.map((item) {
+                            return ListTile(
+                              title: Text(item.titulo),
+                              subtitle: Text(item.descricao),
+                            );
+                          }).toList()
+                      ),
+                    ]
                     ),
 
                   ),
@@ -148,23 +406,6 @@ class HomeMobile extends StatelessWidget {
           ),
         ),
 
-        //Lista More em qualquer lugar
-        SliverList(
-            delegate: SliverChildBuilderDelegate((context, indice){
-              return Container(color: Colors.white, height: 50,);
-            },
-                childCount: 1
-            )
-        ),
-
-        //Lista Fique por dentro
-        SliverList(
-            delegate: SliverChildBuilderDelegate((context, indice){
-              return Container(color: Colors.white, height: 50,);
-            },
-                childCount: 1
-            )
-        ),
       ],
     );
   }
@@ -237,3 +478,4 @@ class HomeDesktop extends StatelessWidget {
     );
   }
 }
+
